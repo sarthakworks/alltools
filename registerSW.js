6a5ed0a3613e9ae65217268010deedc79d9dbd55
sarthak bansal <sarthakb@adobe.com>
2026-02-04 13:45:07 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/alltools/sw.js', { scope: '/alltools/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
